f3c29b89def0f038130737d278b05c89dce8243f
walid <walid.bezzaoui.1@ens.etsmtl.ca>
2017-11-13 16:43:48 -0500

    TP05 INIT

---
+++ log330_tp05/src/test/java/log330_tp05/log330_tp05/CorrelationTest.java
@@ -0,0 +1,75 @@
+package log330_tp05.log330_tp05;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class CorrelationTest {
+
+
+	 public CorrelationTest() {
+
+		}
+
+		/**
+	     * Rigourous Test :-)
+		 * @throws IOException 
+		 * @throws FileNotFoundException 
+		 * @throws ParseException 
+		 * @throws NumberFormatException 
+	     */
+	    @Test
+	    public void correlationTest() throws FileNotFoundException, IOException, NumberFormatException, ParseException
+	    {
+	    	 FileAnalyser fa = new FileAnalyser("data.csv");
+	    	 CalculD c = new CalculD();
+			 c.start(fa.loadFile());
+	    	assertEquals(0.95,c.calculateCorrelation(),0.01);
+	    }
+	    
+	    
+	    @Test
+	    public void correlationMaxTest() throws FileNotFoundException, IOException, NumberFormatException, ParseException
+	    {
+	    	FileAnalyser fa = new FileAnalyser("data.csv");
+	    	List<Points> myList = new ArrayList<Points>();
+	      	myList.add(new Points(22.0, 23.0));
+	    	myList.add(new Points(23.0, 24.0));
+	    	myList.add(new Points(22.0, 23.0));
+	    	CalculD c = new CalculD();
+	    	c.start(myList);
+	    	assertEquals(1.0,c.calculateCorrelation(),0.01);
+	    }
+	    
+	    @Test
+	    public void correlationMinTest() throws FileNotFoundException, IOException, NumberFormatException, ParseException
+	    {
+	    	FileAnalyser fa = new FileAnalyser("data.csv");
+	    	List<Points> myList = new ArrayList<Points>();
+	    	myList.add(new Points(-22.0, -23.0));
+	    	myList.add(new Points(23.0, -24.0));
+	    	myList.add(new Points(-22.0, -23.0));
+	    	CalculD c = new CalculD();
+	    	c.start(myList);
+	    	assertEquals(-1.0,c.calculateCorrelation(),0.01);
+	    }
+	    
+	    
+	    @Test(expected = ParseException.class)
+	    public void correlationTestException() throws FileNotFoundException, IOException, NumberFormatException, ParseException
+	    {
+	  	  FileAnalyser fa = new FileAnalyser("data2.csv");
+	  	  CalculD c = new CalculD();
+	  	  c.start(fa.loadFile());
+	  	  
+	    } 
+	    
+	  
+}
---
+++ log330_tp05/src/test/java/log330_tp05/log330_tp05/VarianceTest.java
@@ -0,0 +1,84 @@
+package log330_tp05.log330_tp05;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class VarianceTest  {
+	
+	
+
+
+
+    public VarianceTest() throws FileNotFoundException, IOException {
+
+	}
+
+    
+    @Test
+    public void varianceMinTest() throws FileNotFoundException, IOException
+    {
+  	  FileAnalyser fa = new FileAnalyser("oldData.csv");
+  	//  List<Double> listElement = fa.loadFile();
+  	 List<Double> listElement = new ArrayList<Double>();
+  	  listElement.add(Double.MIN_VALUE);
+  	  listElement.add(Double.MIN_VALUE);
+  	  CalculD c = new CalculD();
+  	  c.oldStart(listElement);
+  	  assertEquals(0.0,c.calculateVariance(),0.1);
+  	  
+    } 
+    
+
+    @Test
+    public void varianceMaxTest() throws FileNotFoundException, IOException
+    {
+  	  FileAnalyser fa = new FileAnalyser("oldData.csv");
+  	  List<Double> listElement = fa.oldLoadFile();
+  	  listElement.add(Double.MAX_VALUE);
+  	  listElement.add(Double.MAX_VALUE);
+
+  	  listElement.add(Double.MAX_VALUE);
+  	  listElement.add(Double.MAX_VALUE);
+
+  	  CalculD c = new CalculD();
+	  c.oldStart(listElement);
+  	  assertEquals(1.7976931348623157E308,c.calculateVariance(),0.1);
+  	  
+    } 
+    
+    @Test(expected = NumberFormatException.class)
+    public void varianceTestException() throws FileNotFoundException, IOException
+    {
+  	  FileAnalyser fa = new FileAnalyser("data2.csv");
+  	  List<Double> listElement = fa.oldLoadFile();
+  	  CalculD c = new CalculD();
+	  c.oldStart(listElement);
+  	  
+    } 
+    
+    
+  /**
+     * Rigourous Test :-)
+   * @throws IOException 
+   * @throws FileNotFoundException 
+     */
+    @Test
+    public void varianceTest() throws FileNotFoundException, IOException
+    {
+  	  FileAnalyser fa = new FileAnalyser("oldData.csv");
+  	  List<Double> listElement = fa.oldLoadFile();
+  	 CalculD c = new CalculD();
+	  c.oldStart(listElement);
+  	  assertEquals(391417.9,c.calculateVariance(),0.1);
+  	  
+    } 
+    
+
+}
